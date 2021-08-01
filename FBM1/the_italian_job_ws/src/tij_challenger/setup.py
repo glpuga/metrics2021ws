84d4ce70e9825fc17769986c81c50d9cc64347fe
Gerardo Puga <glpuga@ekumenlabs.com>
2021-08-01 15:31:58 -0300

    Changed the package name to tij_challenger

--- FBM1/the_italian_job_ws/src/tij_challenger/setup.py
+++ FBM1/the_italian_job_ws/src/tij_challenger/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['tij_data_view'],
+    packages=['tij_challenger'],
     package_dir={'': 'src'},
 )
 
